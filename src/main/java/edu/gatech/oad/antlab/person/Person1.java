0be944d2993f2a367ec0f32a98069a007a2a16a8
Tim Slanschek <t.slanschek@bluewin.ch>
2018-01-25 19:14:00 -0500

    Initial file

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,9 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  String start = input.substring[2];
+	  String end = input.substring[0,1];
+	  return start + end;
 	}
 	
 	/**
