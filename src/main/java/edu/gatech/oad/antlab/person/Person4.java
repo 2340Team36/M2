1fca0c4c9d227aea31aad2cdf8c1a305f73c8e13
Roshni Ghosh <cs2340team36@gmail.com>
2018-01-27 12:58:54 -0500

    Roshni Ghosh - Person 4 changes

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -18,6 +18,7 @@ public class Person4 {
      */
   public Person4(String pname) {
     name = pname;
+    pname = "Roshni Ghosh";
   }
     /**
      * This method should return a string
@@ -31,7 +32,12 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String output = "";
+
+      for(int i = 0; i < input.length(); i++) {
+        output = output + (char)((int)(input.charAt(i)) + 1);
+      }
+      return output;
     }
     
     /**
